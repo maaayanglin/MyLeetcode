9d18013017a8995ddc7316aee0080b00878f5b6b
maaayanglin <51027962+maaayanglin@users.noreply.github.com>
2019-07-24 23:59:38 +0800

    Update pySolution01.py

--- My_Answers/leetcode35_Search_Insert_Position/pySolution01.py
+++ My_Answers/leetcode35_Search_Insert_Position/pySolution01.py
@@ -2,8 +2,8 @@
 # 内存消耗 :14.2 MB, 在所有 Python3 提交中击败了5.47%的用户
 
 # easy but slow !
+# 思路： 等价于在列表中查找第一个值大于或等于给定值的元素下标，没有则相当于在列表最后插入元素的位置
 
-# 等价于在列表中查找第一个值大于或等于给定值的元素下标，没有则相当于在列表最后插入元素的位置
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         low = 0
