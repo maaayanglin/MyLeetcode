59333d1e9b5eb0f3ad062367e16b4b18679e9458
maaayanglin <51027962+maaayanglin@users.noreply.github.com>
2019-07-25 23:50:41 +0800

    Update pySolution01.py

--- My_Answers/leetcode31_Next_Permutation/pySolution01.py
+++ My_Answers/leetcode31_Next_Permutation/pySolution01.py
@@ -1,5 +1,5 @@
-
-
+# 执行用时 :56 ms, 在所有 Python3 提交中击败了79.51%的用户
+# 内存消耗 :13.8 MB, 在所有 Python3 提交中击败了5.12%的用户
 
 # 思路：
 # 要找到下一个排列数，需要从给定数据的右边开始分析，因为一串数字的右边是低位，能更方便快速的确定下一个排列数。
@@ -13,4 +13,30 @@
 # 然后将target与tmp交换即能满足第一条规定。当然，交换后除最高位外的数字串仍然是降序的。（小于tmp的也小于target，大于target的也大于tmp，两者交换不影响有序性）
 # 最后将nms_main除最高位外的数字串进行翻转，从降序变成升序，即满足上述条件二。此时的整个排列数即为下一个排列数。
 
-
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        ridx = n - 1
+        while ridx > 0 and nums[ridx-1] >= nums[ridx]:
+            ridx -= 1
+        if ridx == 0:
+            nums.reverse()
+            return 
+        else:
+            first_idx = ridx - 1
+            rridx = n - 1
+            while nums[first_idx] >= nums[rridx]:
+                rridx -= 1
+            second_idx = rridx
+            nums[first_idx], nums[second_idx] = nums[second_idx], nums[first_idx]
+            
+            lidx = ridx
+            ridx = n - 1
+            while lidx <= ridx:
+                nums[lidx], nums[ridx] = nums[ridx], nums[lidx]
+                ridx -= 1
+                lidx += 1
+            return 
